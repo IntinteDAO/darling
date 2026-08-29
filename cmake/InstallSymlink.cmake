d4ae4820a3b1fea0728b559b7c425a53103d354d
fervi <fervi@localhost>
2026-08-29 18:00:00 +0200

    cmake: use relative symlinks in InstallSymlink for package relocatability
    Update InstallSymlink macro and top-level CMakeLists.txt to generate relative
    symlinks for /Volumes, /private, tmp, mtab, and machine-id, ensuring compatibility
    with Termux package prefix and non-root installations.

--- cmake/InstallSymlink.cmake
+++ cmake/InstallSymlink.cmake
@@ -1,18 +1,3 @@
-# This macro can be used to install symlinks, which turns out to be
-# non-trivial due to CMake version differences and limitations on how
-# files can be installed when building binary packages.
-#
-# The rule for binary packaging is that files (including symlinks) must
-# be installed with the standard CMake install() macro.
-#
-# The rule for non-binary packaging is that CMake 2.6 cannot install()
-# symlinks, but can create the symlink at install-time via scripting.
-# Though, we assume that CMake 2.6 isn't going to be used to generate
-# packages because versions later than 2.8.3 are superior for that purpose.
-#
-#   _filepath: the absolute path to the file to symlink
-#   _sympath: absolute path of the installed symlink
-
 macro(InstallSymlink _filepath _sympath)
     cmake_parse_arguments(INSTALL_SYMLINK "EXCLUDE_FROM_ALL" "COMPONENT" "" ${ARGN})
 
@@ -31,27 +16,13 @@ macro(InstallSymlink _filepath _sympath)
         set(COMPONENT_ARG "")
     endif()
 
-    if (BINARY_PACKAGING_MODE)
-        execute_process(COMMAND "${CMAKE_COMMAND}" -E create_symlink
-                        ${_filepath}
-                        ${CMAKE_CURRENT_BINARY_DIR}/${_symname})
-        install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${_symname}
-                DESTINATION ${_installdir} ${EXCLUDE_FROM_ALL_ARG} ${COMPONENT_ARG})
-    else ()
-        # scripting the symlink installation at install time should work
-        # for CMake 2.6.x and 2.8.x
-        install(CODE "
-            if (\"\$ENV{DESTDIR}\" STREQUAL \"\")
-                execute_process(COMMAND \"${CMAKE_COMMAND}\" -E make_directory ${_installdir})
-                execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink
-                                ${_filepath}
-                                ${_installdir}/${_symname})
-            else ()
-                execute_process(COMMAND \"${CMAKE_COMMAND}\" -E make_directory \$ENV{DESTDIR}/${_installdir})
-                execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink
-                                ${_filepath}
-                                \$ENV{DESTDIR}/${_installdir}/${_symname})
-            endif ()
-        " ${EXCLUDE_FROM_ALL_ARG} ${COMPONENT_ARG})
-    endif ()
+    install(CODE "
+        if (\"\$ENV{DESTDIR}\" STREQUAL \"\")
+            execute_process(COMMAND mkdir -p ${_installdir})
+            execute_process(COMMAND ln -sf ${_filepath} ${_installdir}/${_symname})
+        else ()
+            execute_process(COMMAND mkdir -p \$ENV{DESTDIR}${_installdir})
+            execute_process(COMMAND ln -sf ${_filepath} \$ENV{DESTDIR}${_installdir}/${_symname})
+        endif ()
+    " ${EXCLUDE_FROM_ALL_ARG} ${COMPONENT_ARG})
 endmacro(InstallSymlink)
